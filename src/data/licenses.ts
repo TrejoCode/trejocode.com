1a9ce0e40af756cfda4c8e123d57a0534b56baa0
Sergio A. Trejo <alejandrotrejocode@gmail.com>
2022-11-08 09:33:23 -0500

    chore(Licenses): ⚙️ From static to data file

---
+++ src/data/licenses.ts
@@ -0,0 +1,49 @@
+/**
+ * @description Datos de muestra para licencias
+ */
+
+export interface InterfaceLicense {
+  title: string;
+  url: string;
+  author: string;
+  link: string;
+}
+
+export const LICENSES_DATA: InterfaceLicense[] = [
+  {
+    title: 'Feather Icons',
+    url: 'https://feathericons.com/',
+    author: 'Cole Bemis',
+    link: 'https://twitter.com/colebemis',
+  },
+  {
+    title: 'Pantalla Del Monitor De Computadora',
+    url: 'https://www.pexels.com/es-es/video/pantalla-del-monitor-de-computadora-2278095/',
+    author: 'Joseph Redfield',
+    link: 'https://www.pexels.com/es-es/@inforexplore/',
+  },
+  {
+    title: 'Navegando colecciones NFT',
+    url: 'https://coverr.co/es/videos/navegando-colecciones-nft-6vbTzmEx8v',
+    author: 'Coverr',
+    link: 'https://coverr.co/es/videos/navegando-colecciones-nft-6vbTzmEx8v',
+  },
+  {
+    title: 'Télefonos móviles 4201543',
+    url: 'https://www.pexels.com/es-es/video/telefono-inteligente-telefono-movil-movil-telefonos-moviles-4201543/',
+    author: 'Foysal Ahmed',
+    link: 'https://www.pexels.com/es-es/@foysal-ahmed-2102283/',
+  },
+  {
+    title: 'Gente trabajando',
+    url: 'https://www.pexels.com/es-es/video/gente-escritorio-trabajando-amigos-4974769/',
+    author: 'Olia Danilevich',
+    link: 'https://www.pexels.com/es-es/@olia-danilevich/',
+  },
+  {
+    title: 'Tailwind CSS',
+    url: 'https://github.com/tailwindlabs/tailwindcss',
+    author: 'Tailwind Labs',
+    link: 'https://github.com/tailwindlabs',
+  },
+];
--- src/pages/licencias.tsx
+++ src/pages/licencias.tsx
@@ -4,6 +4,7 @@
 
 import PageLayout from 'components/Layout';
 import { Icon } from '@trejocode/uikit';
+import { LICENSES_DATA, InterfaceLicense } from 'data/licenses';
 
 const PageLicences = (): JSX.Element => (
   <div className="w-full">
@@ -18,155 +19,32 @@ const PageLicences = (): JSX.Element => (
               revisiones para su desarrollo y actualización continua. Este sitio Web utiliza código abierto
             </p>
             <h3 className="font-semibold text-2xl my-8">Multimedia</h3>
-            <div className="flex flex-col w-1/2 lg:w-1/4 mb-6">
-              <a
-                href="https://feathericons.com/"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Feather Icons
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-              <a
-                href="https://twitter.com/colebemis"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Cole Bemis
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-            </div>
-
-            <div className="flex flex-col w-1/2 lg:w-1/4 my-6">
-              <a
-                href="https://www.pexels.com/es-es/video/pantalla-del-monitor-de-computadora-2278095/"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Pantalla Del Monitor De Computadora
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-              <a
-                href="https://www.pexels.com/es-es/@inforexplore/"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Joseph Redfield
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-            </div>
-
-            <div className="flex flex-col w-1/2 lg:w-1/4 my-6">
-              <a
-                href="https://coverr.co/es/videos/navegando-colecciones-nft-6vbTzmEx8v"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Navegando colecciones NFT
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-              <a
-                href="https://coverr.co/es/videos/navegando-colecciones-nft-6vbTzmEx8v"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Coverr
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-            </div>
-
-            <div className="flex flex-col w-1/2 lg:w-1/4 my-6">
-              <a
-                href="https://www.pexels.com/es-es/video/telefono-inteligente-telefono-movil-movil-telefonos-moviles-4201543/"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Télefonos móviles 4201543
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-              <a
-                href="https://www.pexels.com/es-es/@foysal-ahmed-2102283/"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Foysal Ahmed
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-            </div>
-
-            <div className="flex flex-col w-1/2 lg:w-1/4 my-6">
-              <a
-                href="https://www.pexels.com/es-es/video/gente-escritorio-trabajando-amigos-4974769/"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Gente trabajando
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-              <a
-                href="https://www.pexels.com/es-es/@olia-danilevich/"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Olia Danilevich
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-            </div>
-
-            <div className="flex flex-col w-1/2 lg:w-1/4 my-6">
-              <a
-                href="https://github.com/tailwindlabs/tailwindcss"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Tailwind CSS
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-              <a
-                href="https://github.com/tailwindlabs/tailwindcss"
-                target="_blank"
-                rel="noreferrer"
-                className="leading-8 flex items-center font-medium underline"
-              >
-                Tailwind Labs
-                <span aria-hidden>
-                  <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
-                </span>
-              </a>
-            </div>
+            {LICENSES_DATA?.map(({ url, title, link, author }: InterfaceLicense, key) => (
+              <div key={key} className="flex flex-col w-1/2 lg:w-1/4 mb-6">
+                <a
+                  href={url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="leading-8 flex items-center font-medium underline"
+                >
+                  {title}
+                  <span aria-hidden>
+                    <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
+                  </span>
+                </a>
+                <a
+                  href={link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="leading-8 flex items-center font-medium underline"
+                >
+                  {author}
+                  <span aria-hidden>
+                    <Icon icon="ExternalLink" className="stroke-secondary-base w-4 h-4 ml-1" />
+                  </span>
+                </a>
+              </div>
+            ))}
           </div>
         </div>
       </main>
