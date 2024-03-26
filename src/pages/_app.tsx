d5efaf3236c112471d92dc5dfb87b8033243cc6d
Sergio A. Trejo <alejandrotrejocode@gmail.com>
2024-03-25 23:25:45 -0500

    fix: 🩹 env type to string

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ import 'styles/globals.css';
 
 if (typeof window !== 'undefined') {
   // checks that we are client-side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
     loaded: (posthog) => {
       if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
